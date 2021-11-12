f52de262b4b30700a856781e1ef4cc9d1836508c
drStrange221B <50057970+drStrange221B@users.noreply.github.com>
2021-11-12 13:15:20 -0500

    spring-cloud-config-server

---
+++ Spring-cloud-config-server/src/main/java/com/emrys/SpringCloudClientService1Application.java
@@ -0,0 +1,15 @@
+package com.emrys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudClientService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudClientService1Application.class, args);
+	}
+
+}
